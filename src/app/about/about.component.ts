5777fc8a8575439e0954707c0aa648d1c1aad385
vishalrindhe <rindhevishal32@gmail.com>
2025-11-15 21:54:26 +0530

    inital commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css'],
+    standalone: false
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
