49a8641075ac611e3ccb0be2ff049f502e754479
Vishal Rindhe <vishalv@smartyz.com>
2025-12-03 18:18:13 +0530

    added component testing file

---
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -0,0 +1,62 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {CoursesCardListComponent} from './courses-card-list.component';
+import {CoursesModule} from '../courses.module';
+import {COURSES} from '../../../../server/db-data';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
+import {sortCoursesBySeqNo} from '../home/sort-course-by-seq';
+import {Course} from '../model/course';
+import {setupCourses} from '../common/setup-test-data';
+
+
+
+
+describe('CoursesCardListComponent', () => {
+  let component: CoursesCardListComponent
+  let fixture : ComponentFixture<CoursesCardListComponent>
+  let el: DebugElement
+  beforeEach(async () =>{
+    await TestBed.configureTestingModule({
+      imports : [CoursesModule]
+    })
+    .compileComponents()
+    .then(() =>{
+      fixture = TestBed.createComponent(CoursesCardListComponent)
+      component = fixture.componentInstance
+      el = fixture.debugElement
+    })
+  })
+
+  it("should create the component", () => {
+
+  //  pending();
+  expect(component).toBeTruthy()
+    console.log(component);
+    
+  });
+
+
+  it("should display the course list", () => {
+    component.courses = setupCourses()
+    fixture.detectChanges()
+    const cards = el.queryAll(By.css(".course-card"))
+    console.log(el.nativeElement.outerHTML);
+    console.log(cards);
+    
+    expect(component.courses.length).toBe(12)
+    // expect(component.courses.length > 0).toBeTruthy()
+    // expect(component.courses.length > 0).toBeGreaterThan(0)
+
+  });
+
+
+  it("should display the first course", () => {
+
+      pending();
+
+  });
+
+
+});
+
+
